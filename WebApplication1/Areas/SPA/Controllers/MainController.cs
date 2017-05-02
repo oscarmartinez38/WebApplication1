18f4072f5ed407e2f64b218477ad6df533701828
oscar martinez <oscarmartinez2010@gmail.com>
2017-05-01 20:21:23 -0500

    Lab 33 complete - Create a new Area

---
+++ WebApplication1/Areas/SPA/Controllers/MainController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.ViewModels.SPA;
+using OldViewModel = WebApplication1.ViewModels;
+
+namespace WebApplication1.Areas.SPA.Controllers
+{
+    public class MainController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            MainViewModel v = new MainViewModel();
+            v.UserName = User.Identity.Name;
+            v.FooterData = new OldViewModel.FooterViewModel();
+            v.FooterData.CompanyName = "Oscar Martinez";
+            v.FooterData.Year = DateTime.Now.Year.ToString();
+            return View("Index", v);
+        }
+    }
+}

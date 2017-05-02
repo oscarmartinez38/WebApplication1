66942e3ae8478b9841c4f57d416a6e38570dca70
oscar martinez <oscarmartinez2010@gmail.com>
2017-05-01 21:59:54 -0500

    Lab 35 complete

--- WebApplication1/Areas/SPA/Controllers/MainController.cs
+++ WebApplication1/Areas/SPA/Controllers/MainController.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
 using WebApplication1.ViewModels.SPA;
 using OldViewModel = WebApplication1.ViewModels;
+using WebApplication1.Filters;
 
 namespace WebApplication1.Areas.SPA.Controllers
 {
     public class MainController : Controller
     {
-        
+
         public ActionResult Index()
         {
             MainViewModel v = new MainViewModel();
@@ -20,5 +22,72 @@ namespace WebApplication1.Areas.SPA.Controllers
             v.FooterData.Year = DateTime.Now.Year.ToString();
             return View("Index", v);
         }
+
+        public ActionResult EmployeeList()
+        {
+            EmployeeListViewModel employeeListViewModel = new EmployeeListViewModel();
+            EmployeeBusinessLayer empBal = new EmployeeBusinessLayer();
+            List<Employee> employees = empBal.GetEmployees();
+
+            List<EmployeeViewModel> empViewModels = new List<EmployeeViewModel>();
+
+            foreach (Employee emp in employees)
+            {
+                EmployeeViewModel empViewModel = new EmployeeViewModel();
+                empViewModel.EmployeeName = emp.FirstName + " " + emp.LastName;
+                empViewModel.Salary = emp.Salary.Value.ToString("C");
+                if (emp.Salary > 15000)
+                {
+                    empViewModel.SalaryColor = "yellow";
+                }
+                else
+                {
+                    empViewModel.SalaryColor = "green";
+                }
+                empViewModels.Add(empViewModel);
+            }
+            employeeListViewModel.Employees = empViewModels;
+            return View("EmployeeList", employeeListViewModel);
+        }
+
+        public ActionResult GetAddNewLink()
+        {
+            if (Convert.ToBoolean(Session["IsAdmin"]))
+            {
+                return PartialView("AddNewLink");
+            }
+            else
+            {
+                return new EmptyResult();
+            }
+        }
+
+        [AdminFilter]
+        public ActionResult AddNew()
+        {
+            CreateEmployeeViewModel v = new CreateEmployeeViewModel();
+            return PartialView("CreateEmployee", v);
+        }
+
+        [AdminFilter]
+        public ActionResult SaveEmployee(Employee emp)
+        {
+            EmployeeBusinessLayer empBal = new EmployeeBusinessLayer();
+            empBal.SaveEmployee(emp);
+
+            EmployeeViewModel empViewModel = new EmployeeViewModel();
+            empViewModel.EmployeeName = emp.FirstName + " " + emp.LastName;
+            empViewModel.Salary = emp.Salary.Value.ToString("C");
+            if (emp.Salary > 15000)
+            {
+                empViewModel.SalaryColor = "yellow";
+            }
+            else
+            {
+                empViewModel.SalaryColor = "green";
+            }
+            return Json(empViewModel);
+        }
+
     }
 }
